2934b995f402fd2e347a32c8e61b5dcbb8c84f7e
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2025-07-30 17:23:19 -0400

    Initial code. Introduces numbers tool.

---
+++ src/McpServers/McpServers.Playground/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.Hosting;
+using ModelContextProtocol;
+
+Console.WriteLine("Creating host builder...");
+
+var builder = Host.CreateEmptyApplicationBuilder(settings: null);
+
+Console.WriteLine("Adding MCP tools...");
+
+builder.Services 
+    .AddMcpServer()  
+    .WithStdioServerTransport()   
+    .WithTools();
+
+await builder.Build().RunAsync();
---
+++ src/McpServers/McpServers.Playground/Tools/PatternsTool.cs
@@ -0,0 +1,95 @@
+﻿using System.ComponentModel;
+using ModelContextProtocol.Server;
+
+namespace McpServers.Playground.Tools;
+
+[McpToolType]
+public static class PatternsTool
+{
+    [McpTool, Description("Provides the Fibonnaci sequence, up until stated limit.")]
+    public static string GetFibonacci(string length)
+    {
+        if (!int.TryParse(length, out var limit) || limit < 0)
+            return "You need to ask for a total limit, otherwise it will list until infinity! Please provide a non-negative integer.";
+
+        var sequence = new List<int>();
+
+        int a = 0, b = 1;
+
+        while (a <= limit)
+        {
+            sequence.Add(a);
+            var temp = a + b;
+            a = b;
+            b = temp;
+        }
+
+        return string.Join(", ", sequence);
+    }
+
+    [McpTool, Description("Provides the Tribonacci sequence, up until stated limit.")]
+    public static string GetTribonacci(string length)
+    {
+        if (!int.TryParse(length, out var limit) || limit < 0)
+            return "You need to ask for a total limit, otherwise it will list until infinity! Please provide a non-negative integer.";
+
+        var sequence = new List<int>();
+
+        int a = 0, b = 1, c = 1;
+
+        while (a <= limit)
+        {
+            sequence.Add(a);
+            var temp = a + b + c;
+            a = b;
+            b = c;
+            c = temp;
+        }
+
+        return string.Join(", ", sequence);
+    }
+
+    [McpTool, Description("Provides the Tetranacci sequence, up until stated limit.")]
+    public static string GetTetranacci(string length)
+    {
+        if (!int.TryParse(length, out var limit) || limit < 0)
+            return "You need to ask for a total limit, otherwise it will list until infinity! Please provide a non-negative integer.";
+
+        var sequence = new List<int>();
+
+        int a = 0, b = 0, c = 0, d = 1;
+
+        while (a <= limit)
+        {
+            sequence.Add(a);
+            var temp = a + b + c + d;
+            a = b;
+            b = c;
+            c = d;
+            d = temp;
+        }
+
+        return string.Join(", ", sequence);
+    }
+
+    [McpTool, Description("Provides the Lucas numbers sequence, up until stated limit.")]
+    public static string GetLucas(string length)
+    {
+        if (!int.TryParse(length, out var limit) || limit < 0)
+            return "You need to ask for a total limit, otherwise it will list until infinity! Please provide a non-negative integer.";
+
+        var sequence = new List<int>();
+
+        int a = 2, b = 1;
+
+        while (a <= limit)
+        {
+            sequence.Add(a);
+            var temp = a + b;
+            a = b;
+            b = temp;
+        }
+
+        return string.Join(", ", sequence);
+    }
+}
